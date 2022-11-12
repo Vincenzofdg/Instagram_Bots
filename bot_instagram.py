5e12c796a2c8816ab90cb32a7d812081638420d7
Vincenzofdg <vincenzofdg.dev@gmail.com>
2022-11-12 01:35:54 -0300

    feat: is possible to follow

--- bot_instagram.py
+++ bot_instagram.py
@@ -10,47 +10,59 @@ USER = config['USER']
 PASSWORD = config['PASSWORD']
 
 # Google Chrome 107.0.5304.110
-chromedrive_path = './chromedriver'
-browser = webdriver.Chrome(executable_path=chromedrive_path)
+browser = webdriver.Chrome(executable_path='./chromedriver')
 
-# Declarations
-instagram_url = 'https://www.instagram.com/accounts/login/'
-
-browser.get(instagram_url)
+browser.get('https://www.instagram.com/accounts/login/')
 sleep(2)
 
 # Login
 user = browser.find_element(By.NAME, 'username')
 user.send_keys(USER)
-
 password = browser.find_element(By.NAME, 'password')
 password.send_keys(PASSWORD)
 
 btn_login = browser.find_element(By.CSS_SELECTOR, 'div._abak:nth-child(3)')
 btn_login.click()
 
-sleep(4)
+sleep(3)
 
 btn_not_now_login = browser.find_element(
     By.CSS_SELECTOR, 'button._acan:nth-child(1)')
 btn_not_now_login.click()
 
-sleep(1)
-
 btn_not_now_notifications = browser.find_element(
     By.CSS_SELECTOR, 'button._a9--:nth-child(2)')
 btn_not_now_notifications.click()
 
-sleep(1)
+sleep(4)
 
-browser.get('https://www.instagram.com/rampazzoamanda/')
+# Inside
+hastag_list = ['python', 'bot', 'rock']
+novos_users_seguidos = []
+acc = -1
+seguindo = 0
+likes = 0
+comentarios = 0
 
-sleep(5)
+for hastag in hastag_list:
+    acc += 1
+    browser.get('https://www.instagram.com/explore/tags/' +
+                hastag_list[acc] + '/')
+    sleep(4)
+    primeira_thumb = browser.find_element(
+        By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/article/div[1]/div/div/div[1]/div[1]/a')
+    primeira_thumb.click()
+    sleep(randint(3, 4))
+    try:
+        for _ in range(1, 3):
+            usuario = browser.find_element(
+                By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[1]/div/div/div/span/a').text
+            if usuario not in novos_users_seguidos:
+                if browser.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div/div').text == 'Follow':
+                    browser.find_element(
+                        By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div/div').click()
+                    novos_users_seguidos.append(usuario)
+                    seguindo += 1
 
-browser.get('https://www.instagram.com/carlosferauche/')
-
-sleep(5)
-
-browser.get('https://www.instagram.com/ivelintk/')
-
-sleep(100)
+    except:
+        continue
